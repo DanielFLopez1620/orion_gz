57ad1fad752987edd83187d934c6db8053c4b00a
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2025-03-22 22:32:14 -0500

    GZ: Changed format of GZ launchs

--- orion_gz/launch/gz_ros.launch.py
+++ orion_gz/launch/gz_ros.launch.py
@@ -1,144 +1,103 @@
+# ///////////////////////////// REQUIRED LIBRARIES //////////////////////////////
+# .............................. Python libraries ...............................
 import os
-import yaml
 
+# ............................ Launch dependencies .............................
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
+# ........................... Additional packages dependencies ..................
 from nav2_common.launch import ReplaceString   
 
+# //////////////////////////// GLOBAL DEFINITIONS //////////////////////////////
+ARGS = [
+    DeclareLaunchArgument('camera', default_value='a010',
+        description="Choose a cam for the robot (os30a, astra_s, a010)"),
+    DeclareLaunchArgument('servo',default_value='true',
+        description="Boolean to include or not the servos"),
+    DeclareLaunchArgument('g_mov',default_value='true',
+        description="When using camera a010, whether to include or not G Mov"),
+    DeclareLaunchArgument('rasp', default_value='rpi5',
+        description="Select 4 for Raspberry Pi 4B, or 5 for Raspberry Pi 5"),
+    DeclareLaunchArgument('gazebo',default_value='true',
+        description="True for using gazebo tags, false otherwise"),
+    DeclareLaunchArgument('world', default_value='empty.sdf',
+        description='Specify the world file for Gazebo'),
+    DeclareLaunchArgument('x', default_value='0.0', 
+        description='Initial X position'),
+    DeclareLaunchArgument('y', default_value='0.0',
+        description='Initial Y position'),
+    DeclareLaunchArgument('z', default_value='0.5',
+        description='Initial Z position'),
+    DeclareLaunchArgument('R', default_value='0.0',
+        description='Initial Roll'),
+    DeclareLaunchArgument('P', default_value='0.0',
+        description='Initial Pitch'),
+    DeclareLaunchArgument('Y', default_value='3.1416',
+        description='Initial Yaw'),
+    DeclareLaunchArgument('entity', default_value='orion',
+        description='Entity name or your preferred name for the robot'),
+    DeclareLaunchArgument('ros_bridge', default_value='true',
+        description='Boolean flag to indicate the usage of the ROS-GZ bridge')
+]
 
+# /////////////////////////// LAUNCH DEFINITION /////////////////////////////////
 def generate_launch_description():
-    # ------------------------ Paths definitions ----------------------------
+    # Generate launch descripiton
+    ld = LaunchDescription(ARGS)
+
+    # Path definitions
     pkg_gz = get_package_share_directory('orion_gz')
     spawn_file = os.path.join(pkg_gz, 'launch', 'spawn_robot.launch.py')
     bridge_config_file_path = os.path.join(pkg_gz, 'config', 
-        'ros_gz_bridge.yaml')
+        'ros_gz_bridge.yaml')   
 
-    # --------------------------- Configurations -----------------------------
-    camera = LaunchConfiguration('camera')
-    servo = LaunchConfiguration('servo')
-    g_mov = LaunchConfiguration('g_mov')
-    rasp = LaunchConfiguration('rasp')
-    world = LaunchConfiguration('world')
-    x = LaunchConfiguration('x')
-    y = LaunchConfiguration('y')
-    z = LaunchConfiguration('z')
-    roll = LaunchConfiguration('R')
-    pitch = LaunchConfiguration('P')
-    yaw = LaunchConfiguration('Y')
-
-    # -------------------------- Launch arguments -----------------------------
-    camera_arg = DeclareLaunchArgument(
-        'camera',
-        default_value='a010',
-        description="Choose a cam for the robot (os30a, astra_s, a010)"
-    )
-
-    use_servo_arg = DeclareLaunchArgument(
-        'servo',
-        default_value='true',
-        description="Boolean to include or not the servos"
-    )
-
-    use_g_mov_arg = DeclareLaunchArgument(
-        'g_mov',
-        default_value='true',
-        description="When using camera a010, whether to include or not G Mov"
-    )
-    
-    rasp_arg = DeclareLaunchArgument(
-        'rasp',
-        default_value='rpi5',
-        description="Select 4 for Raspberry Pi 4B, or 5 for Raspberry Pi 5"
-    )
-
-    world_arg = DeclareLaunchArgument(
-        'world',
-        default_value='empty.sdf',
-        description='Specify the world file for Gazebo'
-    )
-
-    x_arg = DeclareLaunchArgument(
-        'x', 
-        default_value='0.0', 
-        description='Initial X position'
-    )
-
-    y_arg = DeclareLaunchArgument(
-        'y',
-        default_value='0.0',
-        description='Initial Y position'
-    )
-
-    z_arg = DeclareLaunchArgument(
-        'z',
-        default_value='0.5',
-        description='Initial Z position'
-    )
-
-    roll_arg = DeclareLaunchArgument(
-        'R',
-        default_value='0.0',
-        description='Initial Roll'
-    )
-
-    pitch_arg = DeclareLaunchArgument(
-        'P',
-        default_value='0.0',
-        description='Initial Pitch'
-    )
-
-    yaw_arg = DeclareLaunchArgument(
-        'Y',
-        default_value='0.0',
-        description='Initial Yaw'
-    )
-
-    # ------------------------ Additional setups -------------------------------
+    # Additional config set up
     bridge_config = ReplaceString(
        source_file=bridge_config_file_path,
-       replacements={'<entity>': 'orion'},
+       replacements={'<entity>': LaunchConfiguration('entity')},
    )
 
 
-    # -------------------------- Includes --------------------------------------
-    spawn_include = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(spawn_file),
-        launch_arguments= {"camera": camera, "servo": servo,
-                            "g_mov": g_mov, "rasp": rasp,
-                            "x": x, "y": y, "z": z,
-                            "roll": roll, "pitch": pitch, 
-                            "yaw": yaw}.items(),
-    )
-    # ------------------------ Nodes --------------------------------------------
-    bridge_node = Node(
-        package='ros_gz_bridge',
-        executable='parameter_bridge',
-        parameters=[{
-            'config_file': bridge_config
-        }],
-        output='screen',
+    # -Include spawn
+    ld.add_action(
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(spawn_file),
+            launch_arguments= {
+                "camera": LaunchConfiguration('camera'),
+                "servo": LaunchConfiguration('servo'),
+                "g_mov": LaunchConfiguration('g_mov'), 
+                "rasp": LaunchConfiguration('rasp'),
+                "x": LaunchConfiguration('x'), 
+                "y": LaunchConfiguration('y'), 
+                "z": LaunchConfiguration('z'),
+                "roll": LaunchConfiguration('R'), 
+                "pitch": LaunchConfiguration('P'), 
+                "yaw": LaunchConfiguration('Y'),
+                "world": LaunchConfiguration('world'),
+                "entity": LaunchConfiguration('entity'), 
+                "ros_bridge": LaunchConfiguration('ros_bridge'),
+            }.items(),
+        )
     )
 
+    # Add additional bridge node
+    ld.add_action( 
+        Node(
+            package='ros_gz_bridge',
+            executable='parameter_bridge',
+            parameters=[{
+                'config_file': bridge_config
+            }],
+            output='screen',
+            condition=IfCondition(LaunchConfiguration('ros_bridge')),
+        )
+    )
 
-    # -------------------------- Nodes ----------------------------------------
-    return LaunchDescription([
-        camera_arg,
-        use_servo_arg,
-        use_g_mov_arg,
-        rasp_arg,
-        world_arg,
-        x_arg,
-        y_arg,
-        z_arg,
-        roll_arg,
-        pitch_arg,
-        yaw_arg,
-        spawn_include,
-        bridge_node,
-    ])
+    return ld
--- orion_gz/launch/spawn_robot.launch.py
+++ orion_gz/launch/spawn_robot.launch.py
@@ -1,150 +1,114 @@
+# ///////////////////////////// REQUIRED LIBRARIES //////////////////////////////
+# .............................. Python libraries ...............................
 import os
 
+# ............................ Launch dependencies .............................
 from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
 
+# //////////////////////////// GLOBAL DEFINITIONS //////////////////////////////
+ARGS = [
+    DeclareLaunchArgument('camera', default_value='a010',
+        description="Choose a cam for the robot (os30a, astra_s, a010)"),
+    DeclareLaunchArgument('servo',default_value='true',
+        description="Boolean to include or not the servos"),
+    DeclareLaunchArgument('g_mov',default_value='true',
+        description="When using camera a010, whether to include or not G Mov"),
+    DeclareLaunchArgument('rasp', default_value='rpi5',
+        description="Select 4 for Raspberry Pi 4B, or 5 for Raspberry Pi 5"),
+    DeclareLaunchArgument('gazebo',default_value='true',
+        description="True for using gazebo tags, false otherwise"),
+    DeclareLaunchArgument('world', default_value='empty.sdf',
+        description='Specify the world file for Gazebo'),
+    DeclareLaunchArgument('x', default_value='0.0', 
+        description='Initial X position'),
+    DeclareLaunchArgument('y', default_value='0.0',
+        description='Initial Y position'),
+    DeclareLaunchArgument('z', default_value='0.5',
+        description='Initial Z position'),
+    DeclareLaunchArgument('R', default_value='0.0',
+        description='Initial Roll'),
+    DeclareLaunchArgument('P', default_value='0.0',
+        description='Initial Pitch'),
+    DeclareLaunchArgument('Y', default_value='3.1416',
+        description='Initial Yaw'),
+    DeclareLaunchArgument('entity', default_value='orion',
+        description='Entity name or your preferred name for the robot'),
+    DeclareLaunchArgument('ros_bridge', default_value='false',
+        description='Boolean flag to indicate the usage of the ROS-GZ bridge')
+]
+
+# ///////////////////////////// LAUNCH DEFINITION /////////////////////////////
 def generate_launch_description():
-    # ------------------------ Paths definitions ----------------------------
+    # Generate launch descripiton
+    ld = LaunchDescription(ARGS)
+
+    # Paths definitions
     pkg_description = get_package_share_directory('orion_description')
     pkg_gz = get_package_share_directory('ros_gz_sim')
     rsp_file = os.path.join(pkg_description, 'launch', 'rsp.launch.py')
     gz_file = os.path.join(pkg_gz, 'launch', 'gz_sim.launch.py')
 
-
-    # --------------------------- Configurations -----------------------------
-    camera = LaunchConfiguration('camera')
-    servo = LaunchConfiguration('servo')
-    g_mov = LaunchConfiguration('g_mov')
-    rasp = LaunchConfiguration('rasp')
-    world = LaunchConfiguration('world')
-    x = LaunchConfiguration('x')
-    y = LaunchConfiguration('y')
-    z = LaunchConfiguration('z')
-    roll = LaunchConfiguration('R')
-    pitch = LaunchConfiguration('P')
-    yaw = LaunchConfiguration('Y')
-
-    # -------------------------- Launch arguments -----------------------------
-    camera_arg = DeclareLaunchArgument(
-        'camera',
-        default_value='a010',
-        description="Choose a cam for the robot (os30a, astra_s, a010)"
+    # Include ORION Robot State Publisher
+    ld.add_action(
+            IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(rsp_file),
+            launch_arguments= {
+                "camera": LaunchConfiguration('camera'), 
+                "servo": LaunchConfiguration('servo'),
+                "g_mov": LaunchConfiguration('g_mov'), 
+                "rasp": LaunchConfiguration('rasp'),
+                "gazebo": 'true'
+            }.items(),
+        )
     )
 
-    use_servo_arg = DeclareLaunchArgument(
-        'servo',
-        default_value='true',
-        description="Boolean to include or not the servos"
+    # Include Gazebo Launch
+    ld.add_action(
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(gz_file),
+            launch_arguments={
+                'gz_args': [f'-r -v 4 ', LaunchConfiguration('world')],
+                'on_exit_shutdown': 'true'
+            }.items()
+        )
     )
 
-    use_g_mov_arg = DeclareLaunchArgument(
-        'g_mov',
-        default_value='true',
-        description="When using camera a010, whether to include or not G Mov"
-    )
-    
-    rasp_arg = DeclareLaunchArgument(
-        'rasp',
-        default_value='rpi5',
-        description="Select 4 for Raspberry Pi 4B, or 5 for Raspberry Pi 5"
-    )
+    # Laucnh clock bridge between ROS and GZ
+    ld.add_action(
+       Node(
+               package='ros_gz_bridge',
+               executable='parameter_bridge',
+               arguments=['/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock'],
+               output='screen',
+               namespace='andino_gz_sim',
+               condition=IfCondition(LaunchConfiguration('ros_bridge')),
+           ),
+   )
 
-    world_arg = DeclareLaunchArgument(
-        'world',
-        default_value='empty.sdf',
-        description='Specify the world file for Gazebo'
-    )
-
-    x_arg = DeclareLaunchArgument(
-        'x', 
-        default_value='0.0', 
-        description='Initial X position'
-    )
-
-    y_arg = DeclareLaunchArgument(
-        'y',
-        default_value='0.0',
-        description='Initial Y position'
-    )
-
-    z_arg = DeclareLaunchArgument(
-        'z',
-        default_value='0.5',
-        description='Initial Z position'
+    # Launch ORION spawner
+    ld.add_action(
+        Node(
+            package='ros_gz_sim',
+            executable='create',
+            arguments=[
+                '-name', LaunchConfiguration('entity'),
+                '-x', LaunchConfiguration('x'),
+                '-y', LaunchConfiguration('y'),
+                '-z', LaunchConfiguration('z'),
+                '-R', LaunchConfiguration('R'),
+                '-P', LaunchConfiguration('P'),
+                '-Y', LaunchConfiguration('Y'),
+                '-topic', 'robot_description',
+                '-allow_renaming', 'false',
+            ],
+            output='screen',
+        )
     )
 
-    roll_arg = DeclareLaunchArgument(
-        'R',
-        default_value='0.0',
-        description='Initial Roll'
-    )
-
-    pitch_arg = DeclareLaunchArgument(
-        'P',
-        default_value='0.0',
-        description='Initial Pitch'
-    )
-
-    yaw_arg = DeclareLaunchArgument(
-        'Y',
-        default_value='0.0',
-        description='Initial Yaw'
-    )
-
-    # -------------------------- Includes --------------------------------------
-    rsp_include = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(rsp_file),
-        launch_arguments= {"camera": camera, "servo": servo,
-                            "g_mov": g_mov, "rasp": rasp,
-                            "gazebo": 'true'}.items(),
-    )
-
-    gazebo_include = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(gz_file),
-        launch_arguments={
-            'gz_args': [f'-r -v 4 ', world],
-            'on_exit_shutdown': 'true'
-        }.items()
-    )
-
-    # ------------------------ Nodes --------------------------------------------
-    spawn_model_node = Node(
-        package='ros_gz_sim',
-        executable='create',
-        arguments=[
-            '-name', 'orion',
-            '-x', x,
-            '-y', y,
-            '-z', z,
-            '-R', roll,
-            '-P', pitch,
-            '-Y', yaw,
-            '-topic', 'robot_description',
-            '-allow_renaming', 'false',
-        ],
-        output='screen',
-    )
-
-
-    # -------------------------- Nodes ----------------------------------------
-    return LaunchDescription([
-        camera_arg,
-        use_servo_arg,
-        use_g_mov_arg,
-        rasp_arg,
-        world_arg,
-        x_arg,
-        y_arg,
-        z_arg,
-        roll_arg,
-        pitch_arg,
-        yaw_arg,
-        rsp_include,
-        gazebo_include,
-        spawn_model_node,
-    ])
+    return ld
